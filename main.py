f60c80bdb4e54a0ebece606d7d7e74cd93f8507c
yorozc <yorozcolop49@gmail.com>
2025-08-31 11:50:19 -0700

    added condiments and veggies to prices dict

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import pyinputplus as pyip
 
 prices = {'bread': {'wheat': 2, 'white': 3, 'sourdough': 5},
           'protein': {'chicken': 12, 'turkey': 15, 'ham': 10, 'tofu': 5},
-          'cheese': {'cheddar': 2, 'swiss': 3, 'mozzarella': 1}}
+          'cheese': {'cheddar': 2, 'swiss': 3, 'mozzarella': 1},
+          'condiments': {'mayo': 0.25, 'mustard': 0.30},
+          'veggies': {'lettuce': 0.10, 'tomato': 0.25}}
 
 
 print("Choose sandwich components.")
@@ -12,4 +14,15 @@ print(f"{bread.capitalize()} bread has been chosen.")
 print("===========================")
 protein = pyip.inputMenu(list(prices['protein']))
 print(f"{protein.capitalize()} has been chosen.")
+print("===========================")
+cheeseChoice = pyip.inputYesNo(prompt="Would you like cheese?\n")
+if cheeseChoice == "yes":
+    print("===========================")
+    cheese = pyip.inputMenu(list(prices['cheese']))
+    print(f"{cheese.capitalize()} has been chosen.")
+else:
+    print("===========================")
+    print("No cheese")
+print("===========================")
+mayo = pyip.inputYesNo(prompt="Would you like mayo?\n")
 print("===========================")
