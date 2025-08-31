aa8ec78769644467359440bab928bdc10d6c386b
yorozc <yorozcolop49@gmail.com>
2025-08-31 11:39:44 -0700

    added inputs for bread and protein

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ import pyinputplus as pyip
 prices = {'bread': {'wheat': 2, 'white': 3, 'sourdough': 5},
           'protein': {'chicken': 12, 'turkey': 15, 'ham': 10, 'tofu': 5},
           'cheese': {'cheddar': 2, 'swiss': 3, 'mozzarella': 1}}
+
+
+print("Choose sandwich components.")
+print("===========================")
+bread = pyip.inputMenu(list(prices['bread']))
+print(f"{bread.capitalize()} bread has been chosen.")
+print("===========================")
+protein = pyip.inputMenu(list(prices['protein']))
+print(f"{protein.capitalize()} has been chosen.")
+print("===========================")
