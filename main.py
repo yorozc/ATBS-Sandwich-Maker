4d06602be2edcc87ab3117ff9889f504f83a5c7c
yorozc <yorozcolop49@gmail.com>
2025-08-30 23:12:14 -0700

    created dictionaries for prices of bread, protein, and cheese

---
+++ main.py
@@ -0,0 +1,5 @@
+import pyinputplus as pyip
+
+prices = {'bread': {'wheat': 2, 'white': 3, 'sourdough': 5},
+          'protein': {'chicken': 12, 'turkey': 15, 'ham': 10, 'tofu': 5},
+          'cheese': {'cheddar': 2, 'swiss': 3, 'mozzarella': 1}}
